63250b7232180045131076c7cc4c67c6da3fe19b
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2021-02-22 18:58:59 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp .vimrc /home/dajanto/
+
+cp .bashrc /home/dajanto/
+
+cp .Xresources /home/dajanto/
+
+cp .xinitrc /home/dajanto/
+
+cp .xprofile /home/dajanto/
+
+cp .config/i3/config /home/dajanto/.config/i3/
+
+cp .config/zathura/zathurarc /home/dajanto/.config/zathura/
+
+echo Finished
