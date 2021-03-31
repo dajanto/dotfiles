3f56eb8c11a67aebeee61ceb89af34c06a76f3a0
dajanto <dajanto.1337@hotmail.de>
2021-03-31 13:13:34 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 
 cp .vimrc /home/dajanto/
 cp .bashrc /home/dajanto/
-cp .xinitrc /home/dajanto/
 cp .xprofile /home/dajanto/
 cp .config/i3/config /home/dajanto/.config/i3/
 cp .config/zathura/zathurarc /home/dajanto/.config/zathura/
