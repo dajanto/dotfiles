5b5838a19d6e9f19f82f38f53e90214c66bfbab3
dajanto <dajanto.1337@hotmail.de>
2021-08-22 13:25:54 +0200

    mkdir beforehand

--- install.sh
+++ install.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-# TODO mkdir beforehand
+printf "\n"
+
+if test -d /home/dajanto/.config/i3/; 
+  then cp .config/i3/config /home/dajanto/.config/i3/; 
+  else mkdir -p /home/dajanto/.config/i3/; cp .config/i3/config /home/dajanto/.config/i3/; fi
+echo "i3 [x]";
+
+if test -d /home/dajanto/.config/zathura/; 
+  then cp .config/zathura/zathurarc /home/dajanto/.config/zathura/; 
+  else mkdir -p /home/dajanto/.config/zathura; cp .config/zathura/zathurarc /home/dajanto/.config/zathura/; fi
+echo "zathura [x]";
+
+if test -d /home/dajanto/.config/i3status/; 
+  then cp .config/i3status/config /home/dajanto/.config/i3status/;
+  else mkdir -p /home/dajanto/.config/i3status/; cp .config/i3status/config /home/dajanto/.config/i3status/; fi
+echo "i3status [x]";
+
+if test -d /home/dajanto/picom/
+  then cp .config/picom/picom.conf /home/dajanto/.config/picom/;
+  else mkdir -p /home/dajanto/.config/picom; cp .config/picom/picom.conf /home/dajanto/.config/picom/; fi
+echo "picom [x]";
 
 cp .vimrc /home/dajanto/
+echo ".vimrc [x]";
 cp .bashrc /home/dajanto/
+echo ".bashrc [x]";
 cp .xprofile /home/dajanto/
-cp .config/i3/config /home/dajanto/.config/i3/
-cp .config/zathura/zathurarc /home/dajanto/.config/zathura/
+echo ".xprofile [x]";
 cp .imwheelrc /home/dajanto/
-cp .config/i3status/config /home/dajanto/.config/i3status/
-cp .config/picom/picom.conf /home/dajanto/.config/picom/
+echo ".imwheelrc [x]";
 
+printf "\n"
 echo Finished
