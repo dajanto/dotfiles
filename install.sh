e6ec407b0ab921df30c07f42829e930070a4e642
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2022-11-24 23:36:53 +0100

    gitignore, install und cleanup

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo Installing...
-
-# TODO osx vs linux vs windows installation
-rsync -av --exclude='install.sh' --exclude='README.md' --exclude='.git' --exclude='windows10' . ~
-
-echo ...finished!
+case "$OSTYPE" in
+  darwin*)
+    echo "OSX detected. Installing..."
+    cp .vimrc ~
+    cp osx/.bash_profile ~ ;;
+  linux*)
+    echo "Linux detected. Installing..."
+    cp .vimrc ~
+    cp .bashrc ~ ;;
+  *)
+    echo "Please install manually for $OSTYPE" ;;
+esac
