6947eb9ae9cfb7f88013d43eba33574cf071abe7
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2023-05-28 16:43:06 +0200

    uwu

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash/
 
-# mkdir -p .config/nvim/
-# cp .config/nvim/* ~
 
 case "$OSTYPE" in
   darwin*)
     echo "OSX detected. Installing..."
     cp .vimrc ~
     cp osx/.bash_profile ~
+    # mkdir -p ~/.config/nvim/
+    # cp .config/nvim/* ~/.config/nvim
     echo "...finished!" ;;
   linux*)
     echo "Linux detected. Installing..."
     cp .vimrc ~
     cp .bashrc ~
+    # mkdir -p ~/.config/nvim/
+    # cp .config/nvim/* ~/.config/nvim
     echo "...finished!" ;;
   *)
     echo "Please install manually for $OSTYPE" ;;
